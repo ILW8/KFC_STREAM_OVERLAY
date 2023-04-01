d569876868777358a8e38bf0434d3fd11755b7fd
Dao Heng Liu <daohengliu@gmail.com>
2023-04-01 15:56:44 +0100

    add finals release

--- JSONs/ComingUp/README.md
+++ JSONs/ComingUp/README.md
@@ -1,22 +1,12 @@
 # `coming_up.json` for Starting Soon overlay
 Download the json for the match being streamed, chuck it in `_data/` and name it `coming_up.json`
 
-| Match ID | Match time         | Link to json                                                                         |
-|----------|--------------------|--------------------------------------------------------------------------------------|
-| 17       | Mar 25 (Sat) 19:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/17.json)  |
-| 18       | Mar 25 (Sat) 15:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/18.json)  |
-| 19       | Mar 25 (Sat) 15:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/19.json)  |
-| 20       | Mar 25 (Sat) 19:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/20.json)  |
-| 21A      | Mar 26 (Sun) 04:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/21A.json) |
-| 21B      | Mar 26 (Sun) 19:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/21B.json) |
-| 21C      | Mar 26 (Sun) 15:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/21C.json) |
-| 21D      | Mar 26 (Sun) 19:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/21D.json) |
-| 22A      | Mar 26 (Sun) 18:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/22A.json) |
-| 22B      | Mar 26 (Sun) 18:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/22B.json) |
-| 22C      | Mar 26 (Sun) 16:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/22C.json) |
-| 22D      | Mar 26 (Sun) 16:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/22D.json) |
-| 23       | Mar 26 (Sun) 19:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/23.json)  |
-| 24       | Mar 25 (Sat) 14:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/SF/24.json)  |
-
-
-
+| Match ID | Match time        | Link to json                                                                        |
+|----------|-------------------|-------------------------------------------------------------------------------------|
+| 25       | Apr 1 (Sat) 19:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/F/25.json)  |
+| 26       | Apr 1 (Sat) 14:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/F/26.json)  |
+| 27A      | Apr 2 (Sun) 03:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/F/27A.json) |
+| 27B      | Apr 2 (Sun) 03:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/F/27B.json) |
+| 27C      | Apr 2 (Sun) 16:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/F/27C.json) |
+| 27D      | Apr 2 (Sun) 16:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/F/27D.json) |
+| 28       | Apr 1 (Sat) 16:00 | [download](https://github.com/ILW8/KFC_STREAM_OVERLAY/releases/download/F/28.json)  |
--- JSONs/ComingUp/gen.py
+++ JSONs/ComingUp/gen.py
@@ -5,23 +5,16 @@ import csv
 # copy this from the ref sheet
 # ,stage,id,date,time,red team,,score,,,blue team,referee,streamer,commentator,commentator,mp link,intro json,,,,W,L,ID1,ID2,mp,bans,,,
 SHEET_CSV = """
-,SF,17,Mar 25 (Sat),19:00,plink,•   ✦,,,✦   •,[K] then...,Miku Hatsune,Xpekade,Crystal Enjoyer,,,"{""time"": 1679770800000, ""red_team"": ""plink"", ""blue_team"": ""[K] then...""}",,L,,,,13,12,,,,,
-,SF,18,Mar 25 (Sat),15:00,Polish Hunxin,•   ✦,,,✦   •,sco gla,Suicune3,Xpekade,,,107574824,"{""time"": 1679756400000, ""red_team"": ""Polish Hunxin"", ""blue_team"": ""sco gla""}",,L,,,,14,11,https://osu.ppy.sh/mp/107574824,HR2,NM3,HD2,
-,SF,19,Mar 25 (Sat),15:00,PEKORA LOVERS,•   ✦,,,✦   •,DJ EBAN KFC ORDER,A secret,Kasumi-sama,,,107574754,"{""time"": 1679756400000, ""red_team"": ""PEKORA LOVERS"", ""blue_team"": ""DJ EBAN KFC ORDER""}",,L,,,,15,10,https://osu.ppy.sh/community/matches/107574754,NM4,DT1,,
-,SF,20,Mar 25 (Sat),19:00,Church's Chicken,•   ✦,,,✦   •,Blue Pants,[K],ilw8,,,,"{""time"": 1679770800000, ""red_team"": ""Church's Chicken"", ""blue_team"": ""Blue Pants""}",,L,,,,16,9,,,,,
-,SF,21A,Mar 26 (Sun),04:00,plink,•   ✦,,,✦   •,Polish Hunxin,,ilw8,,,,"{""time"": 1679803200000, ""red_team"": ""plink"", ""blue_team"": ""Polish Hunxin""}",,P,,,,18,17,,,,,
-,SF,21B,Mar 26 (Sun),19:00,plink,•   ✦,,,✦   •,sco gla,freddiiieeee,Xpekade,,,,"{""time"": 1679857200000, ""red_team"": ""plink"", ""blue_team"": ""sco gla""}",,P,,,,18,17,,,,,
-,SF,21C,Mar 26 (Sun),15:00,[K] then...,•   ✦,,,✦   •,Polish Hunxin,,ilw8,,,,"{""time"": 1679842800000, ""red_team"": ""[K] then..."", ""blue_team"": ""Polish Hunxin""}",,P,,,,18,17,,,,,
-,SF,21D,Mar 26 (Sun),19:00,[K] then...,•   ✦,,,✦   •,sco gla,freddiiieeee,Xpekade,,,,"{""time"": 1679857200000, ""red_team"": ""[K] then..."", ""blue_team"": ""sco gla""}",,P,,,,18,17,,,,,
-,SF,22A,Mar 26 (Sun),18:00,PEKORA LOVERS,•   ✦,,,✦   •,Church's Chicken,A secret,affirmedcheese,affirmedcheese,Crystal Enjoyer,,"{""time"": 1679853600000, ""red_team"": ""PEKORA LOVERS"", ""blue_team"": ""Church's Chicken""}",,P,,,,20,19,,,,,
-,SF,22B,Mar 26 (Sun),18:00,PEKORA LOVERS,•   ✦,,,✦   •,Blue Pants,A secret,affirmedcheese,affirmedcheese,Crystal Enjoyer,,"{""time"": 1679853600000, ""red_team"": ""PEKORA LOVERS"", ""blue_team"": ""Blue Pants""}",,P,,,,20,19,,,,,
-,SF,22C,Mar 26 (Sun),16:00,DJ EBAN KFC ORDER,•   ✦,,,✦   •,Church's Chicken,Kasumi-sama,Kasumi-sama,,,,"{""time"": 1679846400000, ""red_team"": ""DJ EBAN KFC ORDER"", ""blue_team"": ""Church's Chicken""}",,P,,,,20,19,,,,,
-,SF,22D,Mar 26 (Sun),16:00,DJ EBAN KFC ORDER,•   ✦,,,✦   •,Blue Pants,Kasumi-sama,Kasumi-sama,,,,"{""time"": 1679846400000, ""red_team"": ""DJ EBAN KFC ORDER"", ""blue_team"": ""Blue Pants""}",,P,,,,20,19,,,,,
-,SF,23,Mar 26 (Sun),19:00,Hotline Bling,•   ✦,,,✦   •,seal,[K],affirmedcheese,affirmedcheese,Crystal Enjoyer,,"{""time"": 1679857200000, ""red_team"": ""Hotline Bling"", ""blue_team"": ""seal""}",,W,,,,13,14,,,,,
-,SF,24,Mar 25 (Sat),14:00,FLOPINSKI,•   ✦,6,0,✦   •,zeisendash,[K],Rainbowtaves,,,107573344,"{""time"": 1679752800000, ""red_team"": ""FLOPINSKI"", ""blue_team"": ""zeisendash""}",,W,,FLOPINSKI,zeisendash,15,16,https://osu.ppy.sh/community/matches/107573344,HR2,DT2,HR3,DT3
+,F,25,Apr 1 (Sat),19:00,seal,•   ✦,,,✦   •,PEKORA LOVERS,[K],Xpekade,Tycani,shiba,,"{""time"": 1680375600000, ""red_team"": ""seal"", ""blue_team"": ""PEKORA LOVERS""}",,L,,,,23,22,,,,,
+,F,26,Apr 1 (Sat),14:00,zeisendash,•   ✦,6,2,✦   •,Polish Hunxin,HuTaoMyMommy,affirmedcheese,shiba,Crystal Enjoyer,MP Link,"{""time"": 1680357600000, ""red_team"": ""zeisendash"", ""blue_team"": ""Polish Hunxin""}",,L,,zeisendash,Polish Hunxin,24,21,,,,,
+,F,27A,Apr 2 (Sun),03:00,seal,•   ✦,,,✦   •,zeisendash,A secret,ilw8,Tycani,,,"{""time"": 1680404400000, ""red_team"": ""seal"", ""blue_team"": ""zeisendash""}",,P,,,,25,26,,,,,
+,F,27B,Apr 2 (Sun),03:00,seal,•   ✦,,,✦   •,Polish Hunxin,A secret,ilw8,Tycani,,,"{""time"": 1680404400000, ""red_team"": ""seal"", ""blue_team"": ""Polish Hunxin""}",,P,,,,25,26,,,,,
+,F,27C,Apr 2 (Sun),16:00,PEKORA LOVERS,•   ✦,,,✦   •,zeisendash,A secret,affirmedcheese,Crystal Enjoyer,affirmedcheese,,"{""time"": 1680451200000, ""red_team"": ""PEKORA LOVERS"", ""blue_team"": ""zeisendash""}",,P,,,,25,26,,,,,
+,F,27D,Apr 2 (Sun),16:00,PEKORA LOVERS,•   ✦,,,✦   •,Polish Hunxin,A secret,affirmedcheese,Crystal Enjoyer,affirmedcheese,,"{""time"": 1680451200000, ""red_team"": ""PEKORA LOVERS"", ""blue_team"": ""Polish Hunxin""}",,P,,,,25,26,,,,,
+,F,28,Apr 1 (Sat),16:00,Hotline Bling,•   ✦,,,✦   •,FLOPINSKI,[K],Xpekade,Crystal Enjoyer,shiba,,"{""time"": 1680364800000, ""red_team"": ""Hotline Bling"", ""blue_team"": ""FLOPINSKI""}",,W,,,,23,24,,,,,
 """
 
-RELEASE_TAG_NAME = "SF"
+RELEASE_TAG_NAME = "F"
 
 
 def generate():
